0125202ecbc7bfa5c1216c38dcf66cee283ae83a
veilance <christopher.tam@alumni.ubc.ca>
2019-02-09 19:09:26 -0800

    migrations and init seed data with health data of fake patient

--- db/migrations/20160712210838_create_users_table.js
+++ db/migrations/20160712210838_create_users_table.js
@@ -1,7 +1,24 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', function (table) {
     table.increments();
-    table.string('name');
+    table.string('medical_id').unique();
+    table.string('first_name');
+    table.string('last_name');
+    table.string('sex');
+    table.string('city');
+    table.date('date_of_birth');
+    table.string('weight');
+    table.string('blood_type');
+    table.string('height');
+    table.string('allergies');
+    table.string('chronic_health_problems');
+    table.string('approx_alcohol_consumption');
+    table.boolean('period');
+    table.float('blood_pressure');
+    table.string('surgeries');
+    table.string('symptoms');
+    table.string('vaccinations');
+    table.string('medications');
   });
 };
 
--- db/seeds/users.js
+++ db/seeds/users.js
@@ -2,9 +2,7 @@ exports.seed = function(knex, Promise) {
   return knex('users').del()
     .then(function () {
       return Promise.all([
-        knex('users').insert({id: 1, name: 'Alice'}),
-        knex('users').insert({id: 2, name: 'Bob'}),
-        knex('users').insert({id: 3, name: 'Charlie'})
+        knex('users').insert({id: 1, medical_id: '2983579257', first_name: 'Saige', last_name: 'Collins', sex: 'Female', city: 'Prince George', date_of_birth: '1965-06-23', weight: '200 pounds', blood_type: 'O', height: '6 feet', allergies: 'peanuts', chronic_health_problems: 'none', approx_alcohol_consumption: '150ml per week', period: false, blood_pressure: 182, surgeries: 'none', symptoms: 'back pain', vaccinations: 'malaria, flu 2018', medications: 'none' }),
       ]);
     });
 };
